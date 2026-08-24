43d970cfbaa301b527b0fa1eb73bc553435a2c1a
Lucas Esteves Menezes <esteveslucas79@gmail.com>
2026-08-24 06:53:24 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- Projeto---Teste-main/Projeto - Teste/AppGUI.java
+++ Projeto---Teste-main/Projeto - Teste/AppGUI.java
@@ -4,11 +4,8 @@ import java.awt.event.ActionListener;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.net.HttpURLConnection;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.awt.*;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.List;
 
 public class AppGUI {
